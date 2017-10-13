0a7618be3bff22987bb9905682571447de8bf5fa
Cristian Payet <payet91@gmail.com>
2017-10-13 13:04:17 +0200

    Adding configuration

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -18,6 +18,9 @@ class ContactServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/views' => resource_path('views/vendor/contact'),
         ]);
+        $this->publishes([
+            __DIR__.'/config/contact.php' => config_path('contact.php'),
+        ]);
     }
 
     /**
---
+++ src/config/contact.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Mail Driver
+    |--------------------------------------------------------------------------
+    |
+    | Email to send the messages sent by the people who fill the contact form
+    |
+    | Supported: Valid email adress. Example: "john.doe@example.com"
+    |
+    */
+
+    'email' => env('CONTACT_MAIL'),
+
+];
