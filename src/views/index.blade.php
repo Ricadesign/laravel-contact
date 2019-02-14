840ca8386a4a18e0a191fd844d6a1520122aee4c
PayetusKid <payet91@gmail.com>
2019-02-14 14:15:29 +0100

    Update to bootstrap 4

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -1,57 +1,93 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2 text-center">
-                <h1>Contacto</h1>
-                @if(isset($message))
-                  <div class="alert alert-success">
-                    {{ $message }}
-                  </div>
-                @endif
-                <form action="/contact" method="post">
-                  {{ csrf_field() }}
-                  @if($errors->has('name'))
-                    <div class="form-group has-error">
-                      <label class="control-label" for="name">Nombre</label>
-                      <input type="text" name="name" class="form-control" placeholder="Nombre" value="{{ old('name')}}" autofocus>
-                      <span class="help-block">{{ $errors->first('name')}}</span>
-                    </div>
-                  @else
-                  <div class="form-group">
-                    <label class="control-label" for="name">Nombre</label>
-                    <input type="text" name="name" class="form-control" placeholder="Nombre" value="{{ old('name') }}">
-                  </div>
-                  @endif
-                  @if($errors->has('email'))
-                    <div class="form-group has-error">
-                      <label class="control-label" for="email">Email</label>
-                      <input type="text" name="email" class="form-control" placeholder="johndoe@example.com" value="{{old('email')}}" autofocus>
-                      <span class="help-block">{{ $errors->first('email')}}</span>
-                    </div>
-                  @else
-                    <div class="form-group">
-                      <label class="control-label" for="email">Email</label>
-                      <input type="text" name="email" class="form-control" placeholder="johndoe@example.com" value="{{ old('email') }}">
-                    </div>
-                  @endif
-                  @if($errors->has('message'))
-                    <div class="form-group has-error">
-                      <label class="control-label" for="message">Mensaje</label>
-                      <textarea name="message" class="form-control" placeholder="Mensaje" rows="5" autofocus>{{ old('message')}}</textarea>
-                      <span class="help-block">{{ $errors->first('message')}}</span>
-                    </div>
-                  @else
-                    <div class="form-group">
-                      <label class="control-label" for="message">Mensaje</label>
-                      <textarea name="message" class="form-control" placeholder="Mensaje" rows="5">{{ old('message')}}</textarea>
-                    </div>
-                  @endif
-                  <input class="btn btn-default" type="submit" name="" value="Enviar">
-                </form>
-
+<div class="container m-auto">
+    <div class="row justify-content-center">
+        <div class="col-xs-12 col-sm-8 col-md-8 col-lg-8 text-center">
+            <h1>Contacto</h1>
+            @if(isset($message))
+            <div class="alert alert-success">
+              {{ $message }}
             </div>
+            @endif
+          <form class="{{$errors->any() ? '' :  'needs-validation'}}" id="needs-validation" action="/contact" method="post" novalidate>
+            {{ csrf_field() }}
+            <div class="row">
+              <div class="col-lg-6">
+                <div class="form-group">
+                  <label for="name">Nombre</label>
+                  @if($errors->any())
+                    <input type="text" name="name" class="form-control {{$errors->has('name') ? 'is-invalid':'is-valid'}}" id="name" placeholder="Nombres" value="{{ old('name')}}" required>
+                  @else
+                   <input type="text" name="name" class="form-control" id="name" placeholder="Nombres" value="{{ old('name')}}" required>
+                  @endif
+                  <div class="invalid-feedback">
+                    {{$errors->has('name') ?  $errors->first('name') : 'Rellena el campo nombre'}}
+                  </div>
+                  <div class="valid-feedback">
+                    Campo Válido
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-6">
+                <div class="form-group">
+                  <label for="email">Email</label>
+                  @if($errors->any())
+                    <input type="email" name="email" class="form-control {{$errors->has('email') ? 'is-invalid':'is-valid'}}" id="email" placeholder="Email" value="{{old('email')}}" required>
+                  @else
+                    <input type="email" name="email" class="form-control" id="email" placeholder="Email" value="{{old('email')}}" required>
+                  @endif
+                  <div class="invalid-feedback">
+                    {{$errors->has('email') ?  $errors->first('email') : 'Rellena el campo email con un email valido'}}
+                  </div>
+                  <div class="valid-feedback">
+                    Campo Válido
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-lg-12">
+                <div class="form-group">
+                <label for="message">Mensaje</label>
+                @if($errors->any())
+                  <textarea type="text" name="message" class="form-control {{$errors->has('message') ? 'is-invalid':'is-valid'}}" id="message" placeholder="Mensaje" rows="5" required>{{ old('message')}}</textarea>
+                @else
+                  <textarea type="text" name="message" class="form-control" id="message" placeholder="Mensaje" rows="5" required>{{ old('message')}}</textarea>
+                @endif
+                <div class="invalid-feedback">
+                  {{$errors->has('message') ?  $errors->first('message') : 'Rellena el campo mensaje'}}
+                </div>
+                <div class="valid-feedback">
+                  Campo Válido
+                </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-2 send text-center text-lg-left">
+              <button class="btn btn-lg btn-outline-primary" type="submit">{{__('Enviar')}}</button>
+            </div>
+          </form>
         </div>
     </div>
+    <script>
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (function() {
+      'use strict';
+      window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+          form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+          }, false);
+        });
+      }, false);
+    })();
+    </script>
+</div>
 @endsection
