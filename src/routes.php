6858502b7acb680bf1fe710624281ac682c3b9fe
Joel Pérez Alava <jperez@rica.design>
2020-07-01 16:35:36 +0200

    closure removed

--- src/routes.php
+++ src/routes.php
@@ -2,7 +2,5 @@
 
 Route::get('contact', 'Ricadesign\Contact\ContactController@index')->middleware('web');
 Route::post('contact', 'Ricadesign\Contact\ContactController@store')->middleware('web');
-Route::get('/mail', function () {
-    return new Ricadesign\Contact\Mail\MessageSent('Marc', 'test@test.com', 'Esto es el mensaje');
-});
+
 
